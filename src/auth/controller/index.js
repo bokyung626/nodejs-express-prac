641900ea51865c1163829301450e2f5133e7bb87
bokyung626 <ruddl033@naver.com>
2024-05-13 19:00:04 +0900

    [FEAT] 로그인 API 구현

---
+++ src/auth/controller/index.js
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import { AuthSevice } from "../service";
+import { RegisterDTO } from "../dto/register.dto";
+
+class AuthController{
+    authService;
+    router;
+    path="/auth";
+
+    constructor(){
+        this.router = Router();
+        this.authService = new AuthSevice();
+        this.init();
+    }
+    
+    init(){
+        // register,login은 모두 post로 작성한다.
+        // Http method가 아닌 이외의 기능을 사용하는 경우 대부분 post를 사용.
+        this.router.post('/register', this.register.bind(this));
+    }
+
+    async register(req, res, next){
+        try{
+            const body = req.body;
+
+            const { accessToken, refreshToken } = await this.authService.register(new RegisterDTO(body));
+
+            res.status(200).json({
+                accessToken, 
+                refreshToken,
+            });
+        }catch(err){
+            next(err);
+        }
+    }
+}
+
+const authController = new AuthController();
+export default authController;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
 import express,{Router} from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
-import controllers from './controllers';
-``
+import Controllers from './controllers';
+import database from './database';
 
-const app = express();
+(async()=>{ // 바로 실행 함수
+    
+    await database.$connect(); 
+    const app = express();
 
-//요청을 json형태로 받는 데 특화된 라이브러리 express 내장함수
-app.use(cors({origin:"*"})); //cors
-app.use(helmet()); //보안강화
-app.use(express.json()); //다양한 형태의 리퀘스트 바디 확인 가능 
-app.use(express.urlencoded({extended: true, limit:"700mb"})); //http URL 쿼리를 좀 더 쉽게 다룰 수 있게 해줌 //용량 제한 설정 json으로 700mb까지만 받음
+    //요청을 json형태로 받는 데 특화된 라이브러리 express 내장함수
+    app.use(cors({origin:"*"})); //cors
+    app.use(helmet()); //보안강화
+    app.use(express.json()); //다양한 형태의 리퀘스트 바디 확인 가능 
+    app.use(express.urlencoded({extended: true, limit:"700mb"})); //http URL 쿼리를 좀 더 쉽게 다룰 수 있게 해줌 //용량 제한 설정 json으로 700mb까지만 받음
 
-// app.use('/users', UserController.router);
-controllers.forEach((controller)=>{
-    app.use(controller.path, controller.router);
-});
+    // app.use('/users', UserController.router);
+    Controllers.forEach((controller)=>{
+        app.use(controller.path, controller.router);
+    });
 
-// req : 요청 -> Request
-// res : 응답 -> Response 
-app.get("/", (req, res) => {
-    res.send("Node.js 정말 재미있어요!");// 아무 표현 요청이나 응답 다 보낼 수 있음.
-})
+    // req : 요청 -> Request
+    // res : 응답 -> Response 
+    app.get("/", (req, res) => {
+        res.send("Server가 실행 되었습니다.");// 아무 표현 요청이나 응답 다 보낼 수 있음.
+    })
 
-// error middleware
-app.use((err,req,res,next)=>{
-    console.log(err);
+    // error middleware
+    app.use((err,req,res,next)=>{
+        console.log(err);
 
-    res.status(err.status || 500).json({message: err.message || "서버에서 에러가 발생했습니다."}); // err.status가 있으면 출력하고 없으면 500으로
-})
+        res.status(err.status || 500).json({message: err.message || "서버에서 에러가 발생했습니다."}); // err.status가 있으면 출력하고 없으면 500으로
+    })
 
-app.listen(8000, () => { // app.listen(작동포트, 서버 실행 후 실행 될 콜백 함수)
-    console.log("server running... port 8000.");
-})
+    app.listen(8000, () => { // app.listen(작동포트, 서버 실행 후 실행 될 콜백 함수)
+        console.log("server running... port 8000.");
+    })
+
+})();
