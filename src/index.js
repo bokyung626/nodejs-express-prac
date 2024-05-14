553e71d26c39a20ab1bec26f46a505bf56f8e97c
bokyung626 <ruddl033@naver.com>
2024-05-14 22:57:21 +0900

    [chore] create community database
    community 데이터베이스를 생성하고 prisma를 통해 user,post,postlike,tag,comment table을 생성했다.

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { jwtAuth } from './middleware';
 
     //요청을 json형태로 받는 데 특화된 라이브러리 express 내장함수
     app.use(cors({origin:"*"})); //cors
-    app.use(helmet()); //보안강화
+    app.use(helmet()); // 보안강화
     app.use(express.json()); //다양한 형태의 리퀘스트 바디 확인 가능 
     app.use(express.urlencoded({extended: true, limit:"700mb"})); //http URL 쿼리를 좀 더 쉽게 다룰 수 있게 해줌 //용량 제한 설정 json으로 700mb까지만 받음
     app.use(jwtAuth);
@@ -30,7 +30,6 @@ import { jwtAuth } from './middleware';
 
     // error middleware
     app.use((err,req,res,next)=>{
-        console.log(err);
 
         res.status(err.status || 500).json({message: err.message || "서버에서 에러가 발생했습니다."}); // err.status가 있으면 출력하고 없으면 500으로
     })
